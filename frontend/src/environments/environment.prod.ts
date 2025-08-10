cdd0dfa7e7090847a8aed4998ba3ab714bb728cc
Angel Garcia Rumbo <AGARRUM@upv.edu.es>
2025-08-10 17:41:03 +0200

    Configurar dominio personalizado y CNAME

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080/api' // Temporalmente localhost para pruebas
+  apiUrl: 'https://disenosagarcia.com/api' // URL del dominio personalizado
 };
